73d612b2b65e963beeef66e95cc50f6fa77b63d2
Maria Romaguera <mr.romaguera.ch@gmail.com>
2022-03-02 11:10:53 +0100

    initial commit

---
+++ dump/myDb.sql
@@ -0,0 +1,1 @@
+-- Replace this file with your .sql database.
---
+++ www/public/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once "../vendor/autoload.php";
+
+echo "Hola Mundo";
+
+//To work with Twig you will need to create a www/templates folder with an index.html
